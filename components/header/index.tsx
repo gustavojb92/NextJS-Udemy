36d24d5aedd5ec7debacd9646ad1a38052a2493d
Gustavo Jardim de Barros <gustavojb.dsn.pack@alterdata.com.br>
2022-11-06 13:00:25 -0300

    correções da homepage

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,11 +5,12 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className={styles.container}>
-      <div style={{marginLeft:"2rem"}}>
+      <div style={{ marginLeft: "2rem" }}>
         <h1>Seja bem-vindo ao JB DEV</h1>
         <p>
-          Aqui você acompanhra umpouca da minha jornada de comome tornei
-          desenvolvedor e vai ter umpouquinho da noção de como é essa vida
+          Aqui você acompanhara como me tornei desenvolvedor e vai ter um
+          pouquinho da noção de como é a vida de um programador desde os
+          primeiros passos.
         </p>
         <p>Acompanhe abaixo as nossas postagens!</p>
       </div>
