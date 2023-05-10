a30a45c0a34622706d0eb7577e133d2f64cc04e1
Gustavo Jardim de Barros <gustavojb.dsn.pack@alterdata.com.br>
2023-05-10 08:56:32 -0300

    atualizações

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -1,38 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./navigation.module.css";
-import logoPequena from "../../public/assets/logo-pequena.png"
-
-const LINKS = [
-  {
-    name: "Home",
-    path: "/",
-  },
-  {
-    name: "Sobre",
-    path: "/about",
-  },
-];
-
-interface NavAnchorProps {
-  path: string;
-  children: string;
-}
-
-function NavAnchor({ path, children }: NavAnchorProps) {
-  return (
-    <Link href={path}>
-      <a className={styles.navAnchor}>{children}</a>
-    </Link>
-  );
-}
+import logoPequena from "../../public/assets/logo-pequena.png";
 
 export default function Navigation() {
   return (
     <nav className="navbar navbar-expand-lg bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" href="/">
-          <Image width={40} height={40} alt='' src={logoPequena}/>
+          <Image width={40} height={40} alt="" src={logoPequena} />
         </Link>
         <button
           className="navbar-toggler"
@@ -47,11 +22,16 @@ export default function Navigation() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {LINKS.map(({ name, path }) => (
-                <li className="nav-item" key={path}>
-                  <a className="nav-link" href={path}>{name}</a>
-                </li>
-              ))}
+            <li className="nav-item">
+              <span style={{color:"#fff"}} className="nav-link">
+                <Link href="/">HOME</Link>
+              </span>
+            </li>
+            <li className="nav-item">
+              <span style={{color:"#fff"}} className="nav-link">
+                <Link href="/about">SOBRE</Link>
+              </span>
+            </li>
           </ul>
         </div>
       </div>
